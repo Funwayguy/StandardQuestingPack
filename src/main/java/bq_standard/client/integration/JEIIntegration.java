64579edd677cf7a04e80b864bb15d7d5f8922abd
Funwayguy <FunwayguyEmail@gmail.com>
2018-12-23 11:48:24 +1000

    Revert "Merge pull request #78 from alphaest/1.12"
    This reverts commit 85c4a9e414e9a49f85ebad65e341a4738629f656, reversing
    changes made to cebbea543367b276a96c2c6dc330054733ee5b30.

--- src/main/java/bq_standard/client/integration/JEIIntegration.java
+++ src/main/java/bq_standard/client/integration/JEIIntegration.java
@@ -1,5 +1,10 @@
 package bq_standard.client.integration;
 
+import mezz.jei.api.IJeiRuntime;
+import mezz.jei.api.IModPlugin;
+import mezz.jei.api.JEIPlugin;
+import mezz.jei.api.recipe.IFocus;
+import mezz.jei.api.recipe.IFocus.Mode;
 import net.minecraftforge.fml.common.Loader;
 
 public class JEIIntegration
@@ -17,7 +22,42 @@ public class JEIIntegration
 		isJeiLoaded = isJeiLoaded || Loader.isModLoaded("jei");
 		if (isJeiLoaded)
 		{
-			BQStandardJEI.showJEI(obj, mouseClick);
+			Mode m = mouseClick != 1 ? Mode.OUTPUT : Mode.INPUT;
+			BQStandardJEI.jei.getRecipesGui().show(new Focus<>(obj, m));
+		}
+	}
+	
+	private static class Focus<T> implements IFocus<T>
+	{
+		private T value;
+		private Mode mode;
+		
+		public Focus(T value, Mode mode)
+		{
+			this.value = value;
+			this.mode = mode;
+		}
+		@Override
+		public T getValue()
+		{
+			return value;
+		}
+
+		@Override
+		public Mode getMode()
+		{
+			return mode;
+		}
+	}
+	
+	@JEIPlugin
+	public static class BQStandardJEI implements IModPlugin
+	{
+		public static IJeiRuntime jei;
+		@Override
+		public void onRuntimeAvailable(IJeiRuntime jeiRuntime)
+		{
+			jei = jeiRuntime;
 		}
 	}
 }
